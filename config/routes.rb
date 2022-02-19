235b7ea9ba0108b2437b7f0f1b623a553b3b83a7
youdaihoi <yohdai.camel@gmail.com>
2022-02-19 05:55:26 +0000

    top設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   resources :ramens, only: [:new, :create, :index, :show, :destroy]
+  root to: 'ramens#index'
+   resources :ramens, only: [:new, :create, :show, :destroy]
     resources :post_comments, only: [:create, :destroy]
 end
