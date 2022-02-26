efd9fafb6be6395c79b6bf4b18f98307fa6fea20
youdaihoi <yohdai.camel@gmail.com>
2022-02-26 10:04:11 +0000

    お問い合わせ直し

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -5,6 +5,6 @@ class InquiryMailer < ApplicationMailer
 
   def received_email(inquiry)
     @inquiry = inquiry
-    mail(to: inquiry.email, subject: 'お問い合わせを承りました')
+    mail(to: ENV['SEND_MAIL'], subject: 'お問い合わせを承りました')
   end
 end
