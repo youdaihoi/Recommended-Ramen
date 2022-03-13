c5298fab65fbb3889eed20efe9025922abf9589c
youdaihoi <yohdai.camel@gmail.com>
2022-03-13 14:21:12 +0000

    rspec.model.userファイル変更1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "名前とメールアドレスとパスワードがあれば登録できる"
+
+  it "名前がなければ登録できない"
+
+  it "メールアドレスがなければ登録できない"
+
+  it "メールアドレスが重複していたら登録できない"
+
+  it "パスワードがなければ登録できない"
+
+  it "パスワードが暗号化されているか"
+
+
 end
